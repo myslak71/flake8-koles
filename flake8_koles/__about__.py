39616cd16bd3bb166e6d235c916d2413adbba746
myslak71 <43068450+myslak71@users.noreply.github.com>
2019-12-31 10:29:37 +0100

    Add python 3.8 to Travis CI

--- flake8_koles/__about__.py
+++ flake8_koles/__about__.py
@@ -1,11 +1,11 @@
 """Package information module."""
 __title__ = 'flake8-koles'
 __description__ = 'Watch your language young lad! Swears and curses linter.'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'myslak71'
 __author_email__ = 'myslak@protonmail.com'
 __url__ = 'https://github.com/myslak71/flake8-koles'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2019 myslak71'
 __keywords__ = ['linter', 'flake8', 'swears', 'curses', 'extension', 'koles']
-__download_url__ = 'https://github.com/myslak71/flake8-koles/archive/v0.2.0.tar.gz'
+__download_url__ = 'https://github.com/myslak71/flake8-koles/archive/v0.2.1.tar.gz'
